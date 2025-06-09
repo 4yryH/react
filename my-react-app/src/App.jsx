d74dd7129233a7c6e4c173df23873d6930bf071d
Oleg Tishinskikh <oleg1992.06.29@gmail.com>
2025-06-09 13:20:40 +0500

    task-3 React Props

--- my-react-app/src/App.jsx
+++ my-react-app/src/App.jsx
@@ -1,35 +1,18 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import ToDoDynamic from './task-3-react.jsx'
+import UserCard from "./task-5-props.jsx";
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h1>Задание JSX</h1>
+      <ToDoDynamic/>
+      <h1>Задание Props</h1>
+      <UserCard name='Мария' age={25}/>
+      <UserCard name='Олег' age={32}/>
+      <UserCard name='Андрей' age={30}/>
     </>
-  )
+  );
 }
 
 export default App
---
+++ my-react-app/src/task-5-props.jsx
@@ -0,0 +1,10 @@
+function UserCard ({name, age}) {
+  return (
+    <div>
+      <p>Имя: {name}</p>
+      <p>Возраст: {age}</p>
+    </div>
+  );
+}
+
+export default UserCard;
